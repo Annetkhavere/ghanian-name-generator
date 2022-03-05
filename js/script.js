ea7a46ce42d78947122e27421dcb95532089cfd8
Annetkhavere <anneywan5@gmail.com>
2022-03-05 23:18:46 +0300

    declare variables in accordance with the id

---
+++ js/script.js
@@ -0,0 +1,6 @@
+let day = document.getElementById("day");
+let month = document.getElementById("month");
+let year = document.getElementById("year");
+let dayOfWeek = document.getElementById("dayofweek");
+let gender = document.getElementById("gender");
+
