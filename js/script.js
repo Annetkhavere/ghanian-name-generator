53d8a74c3fe357da9e2f0322acec3d01091206b9
Annetkhavere <anneywan5@gmail.com>
2022-03-06 23:23:57 +0300

    create a function for day input

--- js/script.js
+++ js/script.js
@@ -1,6 +1,9 @@
-let day = document.getElementById("day");
-let month = document.getElementById("month");
-let year = document.getElementById("year");
-let dayOfWeek = document.getElementById("dayofweek");
-let gender = document.getElementById("gender");
+function myFunction() {
+  let inpObj = document.getElementById("day");
+  if(!inpObj.checkValidity()) {
+    document.getElementById("demo3").innerHTML = inpObj.validationMessage;
+  } else { 
+    document.getElementById("demo3").innerHTML = "Input Ok"
 
+  }
+}
